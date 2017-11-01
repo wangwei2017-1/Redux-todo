8c0451ff75d5348c6901bd7a17c51413d4a12e50
wangwei2017_1 <1129942397@qq.com>
2017-11-01 22:05:41 +0800

    3、引入bootstrap并且创建组件

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,27 @@
 import React,{Component} from 'react'
+import 'bootstrap/dist/css/bootstrap.css'
+import TodoHeader from './TodoHeader'
+import TodoFooter from './TodoFooter'
+import TodoList from './TodoList'
 export default class App extends Component{
     render(){
         return (
-            <div>
-                App
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-6 col-sm-offset-3">
+                        <div className="panel panel-default">
+                            <div className="panel-heading">
+                                <TodoHeader/>
+                            </div>
+                            <div className="panel-body">
+                                <TodoList/>
+                            </div>
+                            <div className="panel-footer">
+                                <TodoFooter/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
