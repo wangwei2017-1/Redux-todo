80726d405c602584896ede2244320372c55c2d2c
wangwei2017_1 <1129942397@qq.com>
2017-11-04 22:36:48 +0800

    8、删除已完成事项

--- src/components/TodoFooter.js
+++ src/components/TodoFooter.js
@@ -1,10 +1,42 @@
-import React,{Component} from 'react'
-export default class TodoFooter extends Component{
+import React,{Component} from 'react';
+import {connect} from 'react-redux';
+import * as types from '../store/action-types';
+class TodoFooter extends Component{
     render(){
         return (
-            <div> 
-                TodoFooter
+            <div className="row">
+                <div className="col-sm-4">
+                    {
+                        this.props.activeCount>0?<span style={{lineHeight:'34px'}}>你还有{this.props.activeCount}件待办事项</span>:null
+                    }
+                </div>
+                <div className="col-sm-5">
+                    <button className="btn btn-default">全部</button>
+                    <button
+                        style={{marginLeft:5}}
+                        className="btn btn-default">未完成</button>
+                    <button
+                        style={{marginLeft:5}}
+                        className="btn btn-default">已完成</button>
+                </div>
+                <div className="col-sm-3">
+                    {this.props.completedCount>0?
+                        <button
+                            className="btn btn-danger"
+                            onClick={()=>this.props.deleteAllCompleted()}>清除已完成</button>
+                        :null}
+                </div>
             </div>
         )
     }
 }
+
+export default connect(
+    state=>({
+        activeCount:state.todos.filter(item=>!item.completed).length,
+        completedCount:state.todos.filter(item=>item.completed).length
+    }),
+    {
+        deleteAllCompleted:()=>({type:types.DELETE_ALL_COMPLETED})
+    }
+)(TodoFooter);
